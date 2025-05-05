37a41271ec47b80fd04a375b6647cba6369e6e43
JoeGuillory <joeguillory15@gmail.com>
2025-05-05 16:10:04 -0500

    Made Golem Prefabs and worked on enemy pooler
    Added Fire Golem Prefab variant
    
    Added Ice Golem Prefab variant
    
    Added Lightning Golem Prefab variant
    
    Worked on Enemy Pooler Script
    Needs To be tested.
    
    Added Spawn Point prefab and Script

--- Assets/Scripts/Enemy Scripts/EnemyBehavior.cs
+++ Assets/Scripts/Enemy Scripts/EnemyBehavior.cs
@@ -2,18 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Pool;
 
 public class EnemyBehavior : MonoBehaviour
 {
 
     private NavMeshAgent _navMesh;
 
+    private ObjectPool<EnemyBehavior> _pool;
 
+    public ObjectPool<EnemyBehavior> Pool { set { _pool = value; } } 
 
     private void Awake()
     {
         _navMesh = GetComponent<NavMeshAgent>();
-
+        
 
     }
 
@@ -26,7 +29,7 @@ public class EnemyBehavior : MonoBehaviour
         }
     }
 
-
+    
     public void SetDestination(GameObject obj)
     {
         _navMesh.SetDestination(obj.transform.position);
---
+++ Assets/Scripts/EnemyPooler.cs
@@ -0,0 +1,128 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class EnemyPooler : MonoBehaviour
+{
+
+
+    [SerializeField]
+    private EnemyBehavior _baseGolemPrefab;
+    [SerializeField]
+    private EnemyBehavior _fireGolemPrefab;
+    [SerializeField]
+    private EnemyBehavior _iceGolemPrefab;
+    [SerializeField]
+    private EnemyBehavior _lightningGolemPrefab;
+
+    int _baseGolemPoolSize = 100;
+    int _baseGolemMaxPoolSize = 300;
+
+    int _specialGolemPoolSize = 20;
+    int _specialGolemMaxPoolSize = 100;
+
+    public static EnemyPooler instance { get; private set; }
+
+    private ObjectPool<EnemyBehavior> _baseGolemPool;
+    private ObjectPool<EnemyBehavior> _iceGolemPool;
+    private ObjectPool<EnemyBehavior> _fireGolemPool;
+    private ObjectPool<EnemyBehavior> _lightninGolemPool;
+
+
+    public ObjectPool<EnemyBehavior> BaseGolemPool { get { return _baseGolemPool; } }
+    public ObjectPool<EnemyBehavior> IceGolemPool { get { return _iceGolemPool; } }
+    public ObjectPool<EnemyBehavior> FireGolemPool { get { return _fireGolemPool; } }
+    public ObjectPool<EnemyBehavior> LighningGolemPool { get { return _lightninGolemPool; } }
+
+
+
+    private void Awake()
+    {
+        if(instance != null && instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            instance = this;
+        }
+
+        if (!_baseGolemPrefab)
+            Debug.LogError("EnemyPooler: Base Golem prefab missing!");
+
+        if (!_fireGolemPrefab)
+            Debug.LogError("EnemyPooler: Fire Golem prefab missing!");
+
+        if (!_iceGolemPrefab)
+            Debug.LogError("EnemyPooler: Ice Golem prefab missing!");
+
+        if (!_lightningGolemPrefab)
+            Debug.LogError("EnemyPooler: Lightning Golem prefab missing!");
+
+        StartPool(ref _baseGolemPool, CreateBaseGolem,_baseGolemPoolSize,_baseGolemMaxPoolSize);
+        StartPool(ref _iceGolemPool, CreateIceGolem,_specialGolemPoolSize,_specialGolemMaxPoolSize);
+        StartPool(ref _baseGolemPool, CreateFireGolem,_specialGolemPoolSize,_specialGolemMaxPoolSize);
+        StartPool(ref _baseGolemPool, CreateLightningGolem,_specialGolemPoolSize,_specialGolemMaxPoolSize);
+
+    }
+
+
+    #region Pool Functions
+    void StartPool(ref ObjectPool<EnemyBehavior> pool, Func<EnemyBehavior> createFunction,int initsize,int maxsize)
+    {
+        pool = new ObjectPool<EnemyBehavior>(createFunction, OnGetFromPool, OnRelease, OnDestroyPoolObject,false,initsize,maxsize);
+    }
+
+
+    EnemyBehavior CreateBaseGolem()
+    {
+        EnemyBehavior golem = Instantiate(_baseGolemPrefab);
+        golem.Pool = _baseGolemPool;
+        return golem;
+    }
+
+    EnemyBehavior CreateFireGolem()
+    {
+        EnemyBehavior golem = Instantiate(_fireGolemPrefab);
+        golem.Pool = _fireGolemPool;
+        return golem;
+    }
+    EnemyBehavior CreateIceGolem()
+    {
+       EnemyBehavior golem = Instantiate(_iceGolemPrefab);
+        golem.Pool = _iceGolemPool;
+        return golem;
+    }
+    EnemyBehavior CreateLightningGolem()
+    {
+       EnemyBehavior golem = Instantiate(_lightningGolemPrefab);
+        golem.Pool = _lightninGolemPool;
+        return golem;
+    }
+
+    private void OnGetFromPool(EnemyBehavior pooledObject)
+    {
+        pooledObject.gameObject.SetActive(true);
+    }
+
+    private void OnRelease(EnemyBehavior pooledObject)
+    {
+        IHealth objecthealth = pooledObject.GetComponent<IHealth>();
+        if(objecthealth != null)
+        {
+            objecthealth.ResetHealth();
+        }
+
+        pooledObject.gameObject.SetActive(false);
+    }
+
+    private void OnDestroyPoolObject(EnemyBehavior pooledObject)
+    {
+        Destroy(pooledObject.gameObject);
+    }
+    #endregion 
+
+
+}
