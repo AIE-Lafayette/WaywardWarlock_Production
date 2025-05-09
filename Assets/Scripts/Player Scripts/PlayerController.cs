827d296eb0c3f470e7aff5bc8dd3f211d453f8a2
basechase <chase7y@gmail.com>
2025-05-09 10:38:55 -0500

    player faces mouse positon
    player faces mouse positon via raycasting to the ground layer plane. need to fix camera parenting issue

--- Assets/Scripts/Player Scripts/PlayerController.cs
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -6,9 +6,37 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+  
+    //player movement
+
+    [SerializeField]
+    private InputActionReference _move;
+
     [SerializeField]
     public float _playerSpeed;
+
+    private Vector3 _moveDirection;
+
+    private Vector3 _playerVelocity;
+
+    //camera facing
+
+    [SerializeField]
+    public GameObject _targetObject;
+
+    private Vector3 _targetPosition;
+
+    private Vector3 _direction;
+
+    private Ray ray;
+    
+    public Camera _mainCamera;
+
+
+    public LayerMask groundLayer;
+
+
+    //projectile firing
 
     [SerializeField]
     public float _bulletTimer;
@@ -16,33 +44,34 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     public GameObject _projectile;
 
-    [SerializeField]
-    public GameObject _targetObject;
-
-
-    [SerializeField]
-    private InputActionReference _move;
-
-    private Vector3 _relativePosition;
-
-    public Vector3 _mousePosition;
-
-    private Vector3 _moveDirection;
-
-    private Vector3 _playerVelocity;
-  
   
 
 
     void Update()
     {
+        ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        if(Physics.Raycast(ray, out RaycastHit hit, 100f, groundLayer))
+        {
+            _targetPosition = hit.point;
+
+            _direction = _targetPosition - transform.position;
+            _direction.y = 0f;
+
+            if (_direction != Vector3.zero)
+            {
+                Quaternion lookRotation = Quaternion.LookRotation(_direction);
+                transform.rotation = Quaternion.Euler(0f, lookRotation.eulerAngles.y, 0f);
+            }
+
+
+        }
        
-        _mousePosition = Mouse.current.position.ReadValue();
-        _relativePosition = Camera.main.ScreenToWorldPoint(_mousePosition);
+        
 
 
 
-        Debug.Log(_relativePosition.ToString());
+       
         
 
 
