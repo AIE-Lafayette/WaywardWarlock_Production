a632599f3bb465e6637ea028df650f7cdff78e51
basechase <chase7y@gmail.com>
2025-05-12 09:18:37 -0500

    camera is fixed to player
    camera is fixed to player. player is being moved by their mesh instead of their own transform in order for the camera to be fixed properly

--- Assets/Scripts/Player Scripts/PlayerController.cs
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -7,49 +7,22 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
   
-    //player movement
-
+    [SerializeField]
+    public float _bulletTimer;
+    [SerializeField]
+    public float _bulletSpeed;
+    [SerializeField]
+    public GameObject _projectilePrefab;
     [SerializeField]
     private InputActionReference _move;
-
     [SerializeField]
     public float _playerSpeed;
 
     private Vector3 _moveDirection;
-
     private Vector3 _playerVelocity;
-
-    //camera facing
-
-    [SerializeField]
-    public GameObject _targetObject;
-
-    private Vector3 _targetPosition;
-
-    private Vector3 _direction;
-
-    private Ray ray;
-    
-    public Camera _mainCamera;
-
-    public LayerMask groundLayer;
-
-    //projectile firing
-
-    [SerializeField]
-    public float _bulletTimer;
-
-    [SerializeField]
-    public float _bulletSpeed;
-
-    [SerializeField]
-    public GameObject _projectilePrefab;
-
-
-
     private Transform _meshTransform;
-
     private float _angle;
+    public Camera _mainCamera;
 
     private void Start()
     {
@@ -64,45 +37,29 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         
-        ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+       
 
         Vector3 _mousePosition = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
 
-        if(Physics.Raycast(ray, out RaycastHit hit, 100f, groundLayer))
-        {
-            _targetPosition = hit.point;
 
-            _direction = _targetPosition - transform.position;
-            _direction.y = 0f;
-
-            _angle = Vector3.Angle(hit.point, transform.position);
-
-            if (_direction != Vector3.zero)
-            {
-                _meshTransform.rotation = Quaternion.Euler(new Vector3(0, _angle));
-
-                //var bullet = Instantiate(_projectilePrefab, _targetPosition, lookRotation);
-                //bullet.GetComponent<Rigidbody>().velocity = transform.forward * _bulletSpeed;
+        //var bullet = Instantiate(_projectilePrefab, _targetPosition, lookRotation);
+        //bullet.GetComponent<Rigidbody>().velocity = transform.forward * _bulletSpeed;
 
 
 
+      
 
-            }
-
-
-        }
        
+
         
 
 
 
-       
-        
 
 
         _moveDirection = new Vector3(_move.action.ReadValue<Vector2>().x, 0,_move.action.ReadValue<Vector2>().y);
         _playerVelocity = _moveDirection * _playerSpeed * Time.deltaTime;
-        transform.Translate(_playerVelocity, Space.Self);
+        _meshTransform.Translate(_playerVelocity, Space.Self);
 
 
 
