3824bbb5384bcb4b093b327a3a2ba5737ad92fa6
JoeGuillory <joeguillory15@gmail.com>
2025-06-18 11:03:26 -0500

    Fixed the spawn points

--- Assets/Scripts/SpawnPoint.cs
+++ Assets/Scripts/SpawnPoint.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.AI.Navigation;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 
 
 public class SpawnPoint : MonoBehaviour
 {
     private EnemyBehavior golem;
+    private NavMeshAgent _navMesh;
     public float DistanceToPlayer { get { return _distanceToPlayer; } set { _distanceToPlayer = value; } }
 
     private float _distanceToPlayer;
@@ -15,52 +19,65 @@ public class SpawnPoint : MonoBehaviour
         Gizmos.DrawSphere(transform.position,1.5f);
     }
 
+    Vector3 GetNavMesh()
+     {
+        Ray ray = new Ray(transform.position, -transform.up);
+        if (Physics.Raycast(ray, out RaycastHit rayhit, 60))
+        {
+            TerrainCollider _mesh = rayhit.collider.gameObject.GetComponent<TerrainCollider>();
+            if (_mesh != null)
+            {
+                return rayhit.point;
+            }
+        }
+        return transform.localPosition;
+     }
+
+    void SetEnemy(GameObject player)
+    {
+        _navMesh = golem.GetComponent<NavMeshAgent>();
+        _navMesh.enabled = false;
+        golem.transform.position = GetNavMesh();
+        _navMesh.enabled = true;
+        golem.SetTarget = player;
+        EnemyPooler.instance.ActiveList.Add(golem);
+    }
+
     public void Spawn(EnemyType type,GameObject player)
     {
         switch(type)
         {
-            
             case EnemyType.BASE:
             {
-                golem = EnemyPooler.instance.BaseGolemPool.Get();
-                golem.gameObject.transform.localPosition = gameObject.transform.localPosition;
-                golem.SetTarget = player;
-                EnemyPooler.instance.ActiveList.Add(golem);
-                break;
+                    golem = EnemyPooler.instance.BaseGolemPool.Get();
+                    SetEnemy(player);
+                    break;
             }
 
             case EnemyType.ICE:
             {
 
-                golem = EnemyPooler.instance.IceGolemPool.Get();
-                golem.gameObject.transform.localPosition = gameObject.transform.localPosition;
-                golem.SetTarget = player;
-                EnemyPooler.instance.ActiveList.Add(golem);
-                break;
+                    golem = EnemyPooler.instance.IceGolemPool.Get();
+                    SetEnemy(player);
+                    break;
             }
             case EnemyType.FIRE:
             {
-                golem = EnemyPooler.instance.FireGolemPool.Get();
-                golem.gameObject.transform.localPosition = transform.localPosition;
-                golem.SetTarget = player;
-                EnemyPooler.instance.ActiveList.Add(golem);
-                break;
+                    golem = EnemyPooler.instance.FireGolemPool.Get();
+                    SetEnemy(player);
+                    break;
             }
             case EnemyType.LIGHTNING:
             {
 
-                golem = EnemyPooler.instance.LightningGolemPool.Get();
-                golem.gameObject.transform.position = transform.localPosition;
-                golem.SetTarget = player;
-                EnemyPooler.instance.ActiveList.Add(golem);
-                break;
+                    golem = EnemyPooler.instance.LightningGolemPool.Get();
+                    SetEnemy(player);
+                    break;
             }
             default:
-                golem = EnemyPooler.instance.BaseGolemPool.Get();
-                golem.gameObject.transform.localPosition = gameObject.transform.localPosition;
-                golem.SetTarget = player;
-                EnemyPooler.instance.ActiveList.Add(golem);
-                break;
+                    golem = EnemyPooler.instance.BaseGolemPool.Get();
+                    SetEnemy(player);
+                    break;
         }
 
     }
