876d10d91d21f550667831f2fc389688921d7217
JoeGuillory <joeguillory15@gmail.com>
2025-06-23 14:42:17 -0500

    Finished Main Menu

--- Assets/Scripts/Enemy Scripts/EnemyBehavior.cs
+++ Assets/Scripts/Enemy Scripts/EnemyBehavior.cs
@@ -53,7 +53,7 @@ public class EnemyBehavior : MonoBehaviour
     private void Start()
     {
        
-        _placementOffset = new Vector3(0, 1, 0);
+        _placementOffset = new Vector3(0, 2, 0);
         if (!_navMesh)
         {
             Debug.LogError("EnemyBehavior: No instance of NavMeshAgent Component!");
